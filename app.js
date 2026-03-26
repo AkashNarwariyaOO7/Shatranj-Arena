bb566ed431d87a13647557f9a33e1a83498f1294
AkashNarwariyaOO7 <a.k.narwariya777@gmail.com>
2026-03-27 03:29:52 +0530

    updated chess game code

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let playerData = {};
 // let currentPlayer = "w";
 // let lastMove = null;
  
-
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
